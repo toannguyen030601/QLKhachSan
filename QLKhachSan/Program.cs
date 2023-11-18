15b3b367875906df838cb829ffab188033435a34
Toàn <toannguyen030601@gmail.com>
2023-11-18 19:36:31 +0700

    test

--- QLKhachSan/Program.cs
+++ QLKhachSan/Program.cs
@@ -10,6 +10,8 @@ namespace QLKhachSan
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1()); //test123asdas
             ApplicationConfiguration.Initialize(); //123
             Application.Run(new Form1()); //test1
         }
