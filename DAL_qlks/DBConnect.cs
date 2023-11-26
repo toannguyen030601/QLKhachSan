bb3f875753e1c916a46c1e50ff006c317b4cacc3
Nexeki <nexeker@gmail.com>
2023-11-26 18:26:13 +0700

    sửa db giống master

--- DAL_qlks/DBConnect.cs
+++ DAL_qlks/DBConnect.cs
@@ -10,27 +10,7 @@ namespace DAL_qlks
     public class DBConnect
     {
         static string connectionString = "Host=hattie.db.elephantsql.com;Port=5432;Database=ajejkikf;Username=ajejkikf;Password=xMYEdEiVT5W-zlDEI2wXYnK7OkauaTg3;";
-        protected NpgsqlConnection connection;
-        public DBConnect()
-        {
-            connection = new NpgsqlConnection(connectionString);
-        }
-
-        public NpgsqlConnection OpenConnection()
-        {
-            if (connection.State == System.Data.ConnectionState.Closed || connection.State == System.Data.ConnectionState.Broken)
-            {
-                connection.Open();
-            }
-            return connection;
-        }
-
-        public void CloseConnection()
-        {
-            if (connection.State == System.Data.ConnectionState.Open)
-            {
-                connection.Close();
-            }
-        }
+        protected NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        
     }
 }
